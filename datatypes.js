30479d725f1865055cc5aed1642ba4dfa0057933
Heritagecodes <adesokanheritage@gmail.com>
2026-01-09 14:11:46 -0800

    Mobile Dev Jottings

---
+++ datatypes.js
@@ -0,0 +1,14 @@
+// REFERENCE DATA TYPES
+
+// Arrays, Objects and Sets
+//Arrays- An array is a type of data structure that can hold or store multiple types of data e.g number, string, boolean
+let array1 = []
+let agesArray = [10,20,40,50,60,90,100]
+let namesArray = ['Adefolarin', 'James', 'John', 'Mary', 'Linda',]
+let AdefolarinArray = [ "Adefolarin", 30, "male", "red", true]
+
+console.log(agesArray[0]) //This gives the first element in the array as indexing starts from zero
+
+console.log(namesArray)
+//console.log(agesArray.length)
+//console.log(agesArray[agesArray.length -1])
---
+++ intro.js
@@ -0,0 +1,78 @@
+//Variables: There are 3  types; var, let and const
+// var and let are used to declare variables that change while const is used for variables that never change
+
+let favouriteColor = "red"
+//var myName = "Joshua"
+const PI = "3.142"
+console.log(PI)
+
+//There are 2 major data types
+//Fundamental data type: we have 6 of them
+//Reference data type
+
+//Fundamental data type
+// String: combination of characters or a word. It is always in quote.
+// Numbers
+// Booleans
+// Undefined
+// Null
+// Symbol
+//let age = "21" // number
+let isMarried = "false" //boolean
+let favouriteClub;
+//When you assign a variable to a const variable, it would result into an error
+
+
+//Operations on Datatypes
+//Arithmetic Operations
+let x = 10;
+let y = 20;
+let z = x + y;
+
+//Operations on Strings
+// Concatenation
+//let occupation = "Engineer";
+//let favClub = "Barcelona";
+//let occupaClub = occupation + " " + favClub;
+
+//To convert a string to uppercase or lowercase
+//let favClubUpper = favClub.toUpperCase();
+//let favClubLower = favClub.toLowerCase();
+
+//Operations on Booleans
+//There are only 2 values for booleans: true or false
+//logic gates - AND, OR, NOT
+//Boolean is used when we're performing logical operations
+//AND- && Double Ampersand. It's only true if both left and right operands are true
+let jamesAge = 30;
+let standardAge = 18;
+console.log(standardAge && true)
+
+//OR- || is true if either side is true (either left or right operands are true)
+let age = "16"
+let legalAge = 18;
+let gender = "female"
+//console.log(age>=legalAge || gender== "male" && "I can drink alcohol")
+
+// NOT- ! is used to reverse the logical state of its operand
+// That is, it converts true to false and false to true
+
+//console.log(!false)
+//console.log(!true)
+//console.log( !"I love you")
+
+//TRUTHY AND FALSY VALUES
+//Truthy values are values that evaluate to true in a boolean context e.g numbers, strings, objects, arrays
+//Falsy values are values that evaluate to false in a boolean context e.g undefined, null
+
+//console.log(Boolean("Joshua"))
+
+//CONDITIONALS
+
+//if(){
+
+//}
+
+//else{
+
+//}
